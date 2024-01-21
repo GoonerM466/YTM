973ea0a85cd30b1571046b46172a5330761d999a
GoonerM466 <105618642+GoonerM466@users.noreply.github.com>
2024-01-21 17:13:39 -0500

    Update check_channel.sh

--- check_channel.sh
+++ check_channel.sh
@@ -7,55 +7,45 @@ mkdir -p "$output_dir"
 touch "$output_dir/$channel_name.m3u8"
 
 start_time=$(date +%s)
-max_search_time=20
-video_count=0
+max_search_time=25
 max_retries=1
 retry_count=0
 
+# Use timeout to limit the execution time
+timeout "$max_search_time"s bash -c "
 while true; do
-  current_time=$(date +%s)
-  elapsed_time=$((current_time - start_time))
-
-  if [ $elapsed_time -ge $max_search_time ]; then
-    echo "Maximum search time reached. Stopping search for the channel."
-    break
-  fi
-
-  urls=$(yt-dlp --print urls "https://www.youtube.com/@$channel_name/streams" 2>&1)
+  urls=$(yt-dlp --print urls 'https://www.youtube.com/@$channel_name/streams' 2>&1)
+  
   if [ $? -ne 0 ]; then
     error_message=$(echo "$urls" | tail -n 1)
+    
     if [[ $error_message == *"HTTP Error 429: Too Many Requests"* ]]; then
-      echo "Encountered 'HTTP Error 429: Too Many Requests'. Retrying ($((retry_count + 1)) of $max_retries)."
+      echo 'Encountered HTTP Error 429: Too Many Requests. Retrying.'
       retry_count=$((retry_count + 1))
+      
       if [ $retry_count -gt $max_retries ]; then
-        echo "Maximum retries reached. Stopping search for the channel."
+        echo 'Maximum retries reached. Stopping search for the channel.'
         break
       fi
-      sleep 20
       continue
     else
       echo "Error: $error_message"
-      echo "Error while checking the channel. Moving on to the next channel."
+      echo 'Error while checking the channel. Moving on to the next channel.'
       break
     fi
   fi
-
+  
   if echo "$urls" | grep -Eq 'This live event will begin in [0-9]+ (days|hours)'; then
-    echo "Skipping scheduled live event. Moving on to the next channel."
+    echo 'Skipping scheduled live event. Moving on to the next channel.'
     break
   fi
 
   if echo "$urls" | grep -Eq 'This live event has ended'; then
-    echo "Found videos from the past. Moving on to the next channel."
-    break
-  fi
-
-  if [ $video_count -ge 20 ]; then
-    echo "Checked 20 videos, none are live. Moving on to the next channel."
+    echo 'Found videos from the past. Moving on to the next channel.'
     break
   fi
 
   echo "$urls" > "$output_dir/$channel_name.m3u8"
-
-  video_count=$((video_count + 1))
+  break
 done
+"
