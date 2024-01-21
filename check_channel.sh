76288976bf1bd9a7803863a1358026869e10b39b
GoonerM466 <105618642+GoonerM466@users.noreply.github.com>
2024-01-21 16:38:38 -0500

    Update check_channel.sh

--- check_channel.sh
+++ check_channel.sh
@@ -8,6 +8,7 @@ touch "$output_dir/$channel_name.m3u8"
 
 start_time=$(date +%s)
 video_count=0
+
 while true; do
   if yt-dlp --print urls "https://www.youtube.com/@$channel_name/streams" | grep -Eq 'This live event will begin in [0-9]+ (days|hours)'; then
     echo "Skipping scheduled live event. Moving on to the next channel."
@@ -25,8 +26,16 @@ while true; do
     break
   fi
 
-  if yt-dlp --print urls "https://www.youtube.com/@$channel_name/streams"; then
-    break
+  if ! yt-dlp --print urls "https://www.youtube.com/@$channel_name/streams"; then
+    error_message=$(yt-dlp "https://www.youtube.com/@$channel_name/streams" 2>&1)
+    if [[ $error_message == *"HTTP Error 429: Too Many Requests"* ]]; then
+      echo "Encountered 'HTTP Error 429: Too Many Requests'. Stopping search for the channel after one retry."
+      break
+    else
+      echo "Error: $error_message"
+      echo "Error while checking the channel. Moving on to the next channel."
+      break
+    fi
   fi
 
   current_time=$(date +%s)
