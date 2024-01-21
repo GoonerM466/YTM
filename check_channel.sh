f089aa3792810d3beb9849b2ea3fc97062431358
GoonerM466 <105618642+GoonerM466@users.noreply.github.com>
2024-01-21 17:02:28 -0500

    Update check_channel.sh

--- check_channel.sh
+++ check_channel.sh
@@ -7,9 +7,8 @@ mkdir -p "$output_dir"
 touch "$output_dir/$channel_name.m3u8"
 
 start_time=$(date +%s)
+max_search_time=20
 video_count=0
-max_retries=1
-retry_count=0
 
 while true; do
   if yt-dlp --print urls "https://www.youtube.com/@$channel_name/streams" | grep -Eq 'This live event will begin in [0-9]+ (days|hours)'; then
@@ -31,14 +30,8 @@ while true; do
   if ! yt-dlp --print urls "https://www.youtube.com/@$channel_name/streams"; then
     error_message=$(yt-dlp "https://www.youtube.com/@$channel_name/streams" 2>&1)
     if [[ $error_message == *"HTTP Error 429: Too Many Requests"* ]]; then
-      echo "Encountered 'HTTP Error 429: Too Many Requests'. Retrying ($((retry_count + 1)) of $max_retries)."
-      retry_count=$((retry_count + 1))
-      if [ $retry_count -gt $max_retries ]; then
-        echo "Maximum retries reached. Stopping search for the channel."
-        break
-      fi
-      sleep 20
-      continue
+      echo "Encountered 'HTTP Error 429: Too Many Requests'. Stopping search for the channel."
+      break
     else
       echo "Error: $error_message"
       echo "Error while checking the channel. Moving on to the next channel."
@@ -50,8 +43,8 @@ while true; do
   elapsed_time=$((current_time - start_time))
   echo "Elapsed time: $elapsed_time seconds"
 
-  if [ $elapsed_time -ge 60 ]; then
-    echo "Time limit reached. Moving on to the next channel."
+  if [ $elapsed_time -ge $max_search_time ]; then
+    echo "Maximum search time reached. Stopping search for the channel."
     break
   fi
 
