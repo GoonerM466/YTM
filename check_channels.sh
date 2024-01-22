c5372dfaf530736cb6cdd028098ccd0781ddf133
GoonerM466 <105618642+GoonerM466@users.noreply.github.com>
2024-01-22 13:47:28 -0500

    Update check_channels.sh

--- check_channels.sh
+++ check_channels.sh
@@ -3,6 +3,9 @@
 # Read channels from current_channels.txt and update/create the corresponding m3u8 files
 
 while IFS=, read -r channel_name group channel_url; do
+    echo "Processing ${channel_name} (${group})"
+    echo "URL: ${channel_url}"
+
     m3u8_file="./${group,,}/${channel_name}.m3u8"
     mkdir -p "${group,,}"
 
@@ -10,7 +13,7 @@ while IFS=, read -r channel_name group channel_url; do
 #EXTM3U
 #EXT-X-VERSION:3
 #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000
-$({yt-dlp --print "${channel_url}"})
+$(yt-dlp --print "${channel_url}")
 EOL
 
 done < current_channels.txt
