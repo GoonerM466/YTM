d537d53b6731f616f3599ace8e3de6c503ec752b
GoonerM466 <105618642+GoonerM466@users.noreply.github.com>
2024-01-22 12:20:00 -0500

    Update check_channel.sh

--- check_channel.sh
+++ check_channel.sh
@@ -5,9 +5,9 @@ for channel_info in "${channels_yaml[@]}"; do
   group=$(echo "$channel_info" | grep -oP 'group: \K.*')
   url=$(echo "$channel_info" | grep -oP 'url: \K.*')
 
-  live_status=$(yt-dlp --quiet --print live "$url" 2>&1 || echo "false")
+  live_status=$(curl -s "$url" | grep -o '\"isLive\":true')
 
-  if [ "$live_status" = "true" ]; then
+  if [ -n "$live_status" ]; then
     echo "Live stream found for $name! Added to ./$group/$name.m3u8"
     mkdir -p ./$group  # Ensure the group directory exists
     touch ./$group/$name.m3u8
@@ -15,7 +15,7 @@ for channel_info in "${channels_yaml[@]}"; do
       echo "#EXTM3U"
       echo "#EXT-X-VERSION:3"
       echo "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000"
-      yt-dlp --quiet --print urls "$url/live" 2>/dev/null || echo "Error fetching live URLs for $name"
+      curl -s "$url/live" 2>/dev/null || echo "Error fetching live URLs for $name"
     } | sudo tee ./$group/$name.m3u8 > /dev/null
   else
     schedule_time=$(yt-dlp --quiet --print scheduledStartTime "$url" 2>&1 || echo "NA")
