64deffff808a396acf30916037498e8c23af4da2
GoonerM466 <105618642+GoonerM466@users.noreply.github.com>
2024-01-22 11:50:12 -0500

    Update check_channel.sh

--- check_channel.sh
+++ check_channel.sh
@@ -1,18 +1,21 @@
-for channel_info in "${all_channels[@]}"; do
-  IFS=',' read -r channel_name channel_group channel_url <<< "$channel_info"
-  live_status=$(yt-dlp --print live "$channel_url")
+for channel_info in "${channels_yaml[@]}"; do
+  name=$(echo "$channel_info" | grep -oP 'name: \K.*')
+  group=$(echo "$channel_info" | grep -oP 'group: \K.*')
+  url=$(echo "$channel_info" | grep -oP 'url: \K.*')
+
+  live_status=$(yt-dlp --print live "$url" 2>/dev/null || true)
   
   if [ "$live_status" = "true" ]; then
-    echo "Live stream found! Added to $channel_name.m3u8"
-    touch ./$channel_name/$channel_name.m3u8
+    echo "Live stream found! Added to $name.m3u8"
+    touch ./$name/$name.m3u8
     {
       echo "#EXTM3U"
       echo "#EXT-X-VERSION:3"
       echo "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000"
-      yt-dlp --print urls "$channel_url/live"
-    } | sudo tee ./$channel_name/$channel_name.m3u8 > /dev/null
+      yt-dlp --print urls "$url/live"
+    } | sudo tee ./$name/$name.m3u8 > /dev/null
   else
-    schedule_time=$(yt-dlp --print scheduledStartTime "$channel_url")
+    schedule_time=$(yt-dlp --print scheduledStartTime "$url" 2>/dev/null || echo "NA")
     if [ "$schedule_time" != "NA" ]; then
       echo "This channel is not currently live. It will be live at $schedule_time (EST timezone)"
     else
@@ -20,3 +23,4 @@ for channel_info in "${all_channels[@]}"; do
     fi
   fi
 done
+shell: /usr/bin/bash -e {0}
