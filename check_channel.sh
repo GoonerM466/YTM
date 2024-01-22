4113bf3519363166c5d91ebbaa6863e8c0824551
GoonerM466 <105618642+GoonerM466@users.noreply.github.com>
2024-01-22 12:34:59 -0500

    Update check_channel.sh

--- check_channel.sh
+++ check_channel.sh
@@ -7,9 +7,12 @@ for channel_info in "${channels_yaml[@]}"; do
   group=$(echo "$channel_info" | grep -oP 'group: \K.*')
   url=$(echo "$channel_info" | grep -oP 'url: \K.*')
 
-  live_status=$(yt-dlp --print live --youtube-api-key "$api_key" "$url" 2>/dev/null || true)
+  live_status=$(yt-dlp --print live --youtube-api-key "$api_key" "$url" 2>&1 || true)
   
-  if [ "$live_status" = "true" ]; then
+  if [[ $live_status == *"This live event will begin"* ]]; then
+    schedule_time=$(yt-dlp --print scheduledStartTime --youtube-api-key "$api_key" "$url" 2>/dev/null || echo "NA")
+    echo "This channel is not currently live. It will be live at $schedule_time (EST timezone)"
+  elif [ "$live_status" = "true" ]; then
     echo "Live stream found! Added to $group/$name.m3u8"
     touch ./$group/$name/$name.m3u8
     {
@@ -19,11 +22,6 @@ for channel_info in "${channels_yaml[@]}"; do
       yt-dlp --print urls --youtube-api-key "$api_key" "$url/live"
     } | sudo tee ./$group/$name/$name.m3u8 > /dev/null
   else
-    schedule_time=$(yt-dlp --print scheduledStartTime --youtube-api-key "$api_key" "$url" 2>/dev/null || echo "NA")
-    if [ "$schedule_time" != "NA" ]; then
-      echo "This channel is not currently live. It will be live at $schedule_time (EST timezone)"
-    else
-      echo "This channel is not currently live."
-    fi
+    echo "This channel is not currently live."
   fi
 done
