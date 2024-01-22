12e765ce0ba11d842578427c24b861accb946819
GoonerM466 <105618642+GoonerM466@users.noreply.github.com>
2024-01-22 12:06:13 -0500

    Update check_channel.sh

--- check_channel.sh
+++ check_channel.sh
@@ -5,7 +5,7 @@ for channel_info in "${channels_yaml[@]}"; do
   group=$(echo "$channel_info" | grep -oP 'group: \K.*')
   url=$(echo "$channel_info" | grep -oP 'url: \K.*')
 
-  live_status=$(yt-dlp --quiet --print live "$url" 2>/dev/null || echo "false")
+  live_status=$(yt-dlp --quiet --print live "$url" 2>/dev/null || true)
   
   if [ "$live_status" = "true" ]; then
     echo "Live stream found! Added to $name.m3u8"
@@ -16,14 +16,14 @@ for channel_info in "${channels_yaml[@]}"; do
       echo "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000"
       yt-dlp --quiet --print urls "$url/live" 2>/dev/null || echo "Error fetching live URLs"
     } | sudo tee ./$name/$name.m3u8 > /dev/null
-  elif [ "$live_status" = "false" ]; then
-    echo "This channel is not currently live."
-  else
+  elif [ "$live_status" != "false" ]; then
     schedule_time=$(yt-dlp --quiet --print scheduledStartTime "$url" 2>/dev/null || echo "NA")
     if [ "$schedule_time" != "NA" ]; then
       echo "This channel is not currently live. It will be live at $schedule_time (EST timezone)"
     else
       echo "Error checking live status for $name"
     fi
+  else
+    echo "This channel is not currently live."
   fi
 done
