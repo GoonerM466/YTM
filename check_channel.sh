fc05b7559f520fe3fbb206498f528e202869bc1a
GoonerM466 <105618642+GoonerM466@users.noreply.github.com>
2024-01-22 11:43:07 -0500

    Update check_channel.sh

--- check_channel.sh
+++ check_channel.sh
@@ -1,28 +1,22 @@
-#!/bin/bash
-
-function update_channel() {
-  IFS=',' read -r channel_name channel_group channel_url <<< "$1"
-
+for channel_info in "${all_channels[@]}"; do
+  IFS=',' read -r channel_name channel_group channel_url <<< "$channel_info"
   live_status=$(yt-dlp --print live "$channel_url")
-
+  
   if [ "$live_status" = "true" ]; then
-    echo "Live stream found for $channel_name! Added to $channel_name.m3u8 in $channel_group"
-    mkdir -p ./$channel_group
-    touch ./$channel_group/$channel_name.m3u8
-    cat > ./$channel_group/$channel_name.m3u8 <<EOL
-#EXTM3U
-#EXT-X-VERSION:3
-#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000
-$(yt-dlp --print urls "$channel_url/live")
-EOL
+    echo "Live stream found! Added to $channel_name.m3u8"
+    touch ./$channel_name/$channel_name.m3u8
+    {
+      echo "#EXTM3U"
+      echo "#EXT-X-VERSION:3"
+      echo "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000"
+      yt-dlp --print urls "$channel_url/live"
+    } | sudo tee ./$channel_name/$channel_name.m3u8 > /dev/null
   else
     schedule_time=$(yt-dlp --print scheduledStartTime "$channel_url")
-    echo "This channel is not currently live. It will be live at $schedule_time (EST timezone)"
+    if [ "$schedule_time" != "NA" ]; then
+      echo "This channel is not currently live. It will be live at $schedule_time (EST timezone)"
+    else
+      echo "This channel is not currently live."
+    fi
   fi
-}
-
-# Example usage:
-update_channel "beinsportshaber,spor,https://www.youtube.com/@beinsportsturkiye"
-update_channel "AFCAsianCup,spor,https://www.youtube.com/@AFCAsianCup"
-update_channel "talkSPORT,spor,https://www.youtube.com/@talkSPORT"
-# Add more channels as needed
+done
