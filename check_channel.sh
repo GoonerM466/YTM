f092698c779ca5d8d6016aaa22cf5f3b765ccfd8
GoonerM466 <105618642+GoonerM466@users.noreply.github.com>
2024-01-22 12:57:34 -0500

    Update check_channel.sh

--- check_channel.sh
+++ check_channel.sh
@@ -1,16 +1,16 @@
-channels_yaml=$(cat ytm.yaml)
+#!/usr/bin/bash -e
 
-api_key="AIzaSyBztHpAhFSfGbFvIkPrcPE9HbhXjQo_tSc"
+channels_yaml=$(cat ytm.yaml)
 
 for channel_info in "${channels_yaml[@]}"; do
   name=$(echo "$channel_info" | grep -oP 'name: \K.*')
   group=$(echo "$channel_info" | grep -oP 'group: \K.*')
   url=$(echo "$channel_info" | grep -oP 'url: \K.*')
 
-  live_status=$(yt-dlp --print live --youtube-api-key "$api_key" "$url" 2>/dev/null || true)
-  
+  live_status=$(yt-dlp --print live "$url" 2>/dev/null || true)
+
   if [[ $live_status == *"This live event will begin"* ]]; then
-    schedule_time=$(yt-dlp --print scheduledStartTime --youtube-api-key "$api_key" "$url" 2>/dev/null || echo "NA")
+    schedule_time=$(yt-dlp --print scheduledStartTime "$url" 2>/dev/null || echo "NA")
     echo "This channel is not currently live. It will be live at $schedule_time (EST timezone)"
   elif [ "$live_status" = "true" ]; then
     echo "Live stream found! Added to $group/$name.m3u8"
@@ -20,7 +20,7 @@ for channel_info in "${channels_yaml[@]}"; do
       echo "#EXTM3U"
       echo "#EXT-X-VERSION:3"
       echo "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000"
-      yt-dlp --print urls --youtube-api-key "$api_key" "$url/live"
+      yt-dlp --print urls "$url/live"
     } | sudo tee ./$group/$name/$name.m3u8 > /dev/null
   else
     echo "This channel is not currently live."
