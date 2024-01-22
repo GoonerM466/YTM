5981370afc5d4be438b734c4d8a08722cb09cab4
GoonerM466 <105618642+GoonerM466@users.noreply.github.com>
2024-01-22 13:49:14 -0500

    Update check_channels.sh

--- check_channels.sh
+++ check_channels.sh
@@ -9,11 +9,14 @@ while IFS=, read -r channel_name group channel_url; do
     m3u8_file="./${group,,}/${channel_name}.m3u8"
     mkdir -p "${group,,}"
 
+    # Use yt-dlp to directly extract video URL
+    video_url=$(yt-dlp --get-url "${channel_url}")
+
     cat >"${m3u8_file}" <<EOL
 #EXTM3U
 #EXT-X-VERSION:3
 #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000
-$(yt-dlp --print "${channel_url}")
+${video_url}
 EOL
 
 done < current_channels.txt
