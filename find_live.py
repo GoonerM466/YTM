c18b192630eda92e7a846b377b2fae6a6f060125
GoonerM466 <105618642+GoonerM466@users.noreply.github.com>
2024-01-23 03:41:13 -0500

    Update find_live.py

--- find_live.py
+++ find_live.py
@@ -5,6 +5,7 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 import yt_dlp
 import time
+from datetime import datetime
 
 MAX_RUNTIME_SECONDS = 210  # 3.5 minutes
 
@@ -15,6 +16,9 @@ def search_live_channels(api_key, max_results=50):
     live_links = []
     next_page_token = None
 
+    # Set the publishedBefore parameter to the current time in RFC 3339 format
+    published_before = datetime.utcnow().isoformat() + "Z"
+
     while True:
         try:
             print("Searching...")
@@ -24,7 +28,10 @@ def search_live_channels(api_key, max_results=50):
                 maxResults=max_results,
                 order="viewCount",
                 type="video",
-                pageToken=next_page_token
+                pageToken=next_page_token,
+                publishedBefore=published_before,
+                regionCode="CA",  # ISO 3166-1 alpha-2 country code for Canada
+                relevanceLanguage="en",  # ISO 639-1 two-letter language code for English
             )
 
             response = request.execute()
@@ -75,45 +82,3 @@ def search_live_channels(api_key, max_results=50):
                 raise
 
     return live_links
-
-def get_channel_category(channel_id):
-    ydl_opts = {
-        'quiet': True,
-    }
-
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        result = ydl.extract_info(f'https://www.youtube.com/channel/{channel_id}', download=False)
-        return result.get('categories', [])[0].lower() if result.get('categories') else None
-
-def write_to_file(live_channels):
-    live_channels.sort(key=lambda x: (x['group'], x['name']))
-
-    with open('all_live_channels.txt', 'w') as file:
-        current_group = None
-
-        for channel in live_channels:
-            if channel['group'] != current_group:
-                file.write(f'\n#####_{channel["group"]}_#####\n')
-                current_group = channel['group']
-
-        # Truncate the file before writing new entries
-        file.truncate(0)
-
-        for channel in live_channels:
-            file.write(f'New! {channel["name"]}, {channel["group"]}, {channel["url"]}\n')
-
-def print_live_channels(live_channels):
-    for channel in live_channels:
-        print(f'New! {channel["name"]}, {channel["group"]}, {channel["url"]}')
-
-if __name__ == '__main__':
-    youtube_api_key = "AIzaSyBztHpAhFSfGbFvIkPrcPE9HbhXjQo_tSc"  # Replace with your actual YouTube API key
-    live_links = search_live_channels(youtube_api_key)
-    
-    if live_links:
-        print("Live Channels:")
-        print_live_channels(live_links)
-        write_to_file(live_links)
-        print("Script executed successfully.")
-    else:
-        print("No live channels found.")
