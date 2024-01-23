2c7d9b8c8dc27f0b743504d3ff8f01dd915a92f7
GoonerM466 <105618642+GoonerM466@users.noreply.github.com>
2024-01-23 10:21:33 -0500

    Update generate_channel_info.py

--- generate_channel_info.py
+++ generate_channel_info.py
@@ -1,52 +1,74 @@
-name: Generate XMLTV
+name: Get YouTube Live m3u8
 
 on:
+  schedule:
+    - cron: '45 0/2 * * *'
   workflow_dispatch:
   workflow_run:
     workflows:
-      - "Get YouTube Live m3u8"
-      - "Clean Old EPG"
+      - "New Channel Scrape Workflow"
+      - "Delete M3U8 Files"
     types:
       - completed
 
 jobs:
-  generate_xmltv:
+  build:
     runs-on: ubuntu-latest
-
     steps:
-    - name: Checkout repository
-      uses: actions/checkout@v2
-
-    - name: Set up Python
-      uses: actions/setup-python@v2
-      with:
-        python-version: 3.8
-
-    - name: Install dependencies
+    - uses: actions/checkout@v2
+    - name: config
       run: |
-        python -m pip install --upgrade pip
-
-    - name: Execute generate_channel_info.py
-      run: python generate_channel_info.py > combined_epg.xml
-
-    - name: Check if changes exist
-      id: check_changes
+        git config --global user.email "action@github.com"
+        git config --global user.name "GitHub Action"
+    - name: Install yt-dlp
       run: |
-        git diff --exit-code || echo "Changes detected" && exit 0
+        curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+        chmod a+rx /usr/local/bin/yt-dlp
+    - name: Delay for 10 seconds
+      run: sleep 10
 
-    - name: Check repository status before staging
-      run: git status
+    - name: Pull latest changes
+      run: git pull origin main
 
-    - name: Stage changes
-      if: steps.check_changes.outputs.return-code == '0'
-      run: git add .
+    - name: Rebase changes
+      run: git pull --rebase origin main
 
-    - name: Check repository status after staging
-      run: git status
+    - name: Create current_channels directory
+      run: mkdir -p ./current_channels
 
-    - name: Commit and push changes
-      if: steps.check_changes.outputs.return-code == '0'
+    - name: Read channel information from current_channels.txt
       run: |
-        git config --local user.email "action@github.com" 2> /dev/null || true
-        git config --local user.name "GitHub Action" 2> /dev/null || true
-        git commit -m "Generate XMLTV" && git push
+        # Clear the contents of live_status.txt
+        echo "" > live_status.txt
+
+        while IFS=, read -r channel_name group channel_url; do
+          # Modify channel_name: remove spaces and trim to 3 words
+          channel_name=$(echo "$channel_name" | sed -E 's/[^a-zA-Z0-9]+/-/g' | awk '{print $1; for(i=2;i<=3;i++) {if($i!="") {printf "-%s", $i}}}')
+          mkdir -p "./current_channels/$group"
+          touch "./current_channels/$group/$channel_name.m3u8"
+          {
+            echo "#EXTM3U"
+            echo "#EXT-X-VERSION:3"
+            echo "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000"
+            yt-dlp --print urls "$channel_url"
+          } | sudo tee "./current_channels/$group/$channel_name.m3u8" >/dev/null
+          
+          # Check if the channel is live
+          live_status="Not Live"
+          if yt-dlp --print is_live "$channel_url" | grep -q "True"; then
+            live_status="Live"
+            echo "$channel_name is live! Updating $channel_name.m3u8 with current live stream!"
+          fi
+          
+          # Append live status information to live_status.txt
+          echo "$channel_name - $live_status - $(date)" >> live_status.txt
+        done < current_channels.txt
+
+    - name: git add
+      run: |
+        git add -A
+        ls -la
+    - name: commit & push
+      run: |
+        git diff --cached --quiet || git commit -m Updated
+        git push origin main
